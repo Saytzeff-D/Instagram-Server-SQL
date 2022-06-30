99afe579284e208082bdf6eada4de69ca3a8141f
Saytzeff-D <davidatanda2020@gmail.com>
2022-06-30 16:59:14 +0100

    first deployment

--- index.js
+++ index.js
@@ -10,5 +10,14 @@ app.use(cors())
 const router = require('./routes/route')
 app.use('/', router)
 
+const pool = require('./connection/connectionPool')
+pool.getConnection((err, connection)=>{
+    if(err) { 
+        console.log('Unable to set Connection to Database') 
+    }else{
+        console.log('Database Connection Established')
+    }
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Instagram Server is listening on port ${port}. Open your Browser to http://localhost:7000 to view.`))
